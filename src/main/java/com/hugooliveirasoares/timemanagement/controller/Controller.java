af76b98e653cddc30f201bccc3a0baa3055abf20
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2020-11-01 20:33:15 -0300

    Saves the Activity using the class Service.saveActivity and return the list of activities

--- src/main/java/com/hugooliveirasoares/timemanagement/controller/Controller.java
+++ src/main/java/com/hugooliveirasoares/timemanagement/controller/Controller.java
@@ -1,6 +1,8 @@
 package com.hugooliveirasoares.timemanagement.controller;
 
 import com.hugooliveirasoares.timemanagement.domain.Activity;
+import com.hugooliveirasoares.timemanagement.domain.ActivityList;
+import com.hugooliveirasoares.timemanagement.service.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,21 +13,23 @@ import java.util.Date;
 @RestController
 public class Controller {
 
+    Service service = new Service();
+
     @GetMapping("/")
     public String getDate(){
         return new Date().toString();
     }
 
     @GetMapping("/getactivity")
-    public Activity getActivity(){
-        return new Activity();
+    public ActivityList getActivity(){
+        return service.getActivityList();
     }
 
     @PostMapping("/postactivity")
     public Activity postactivity(@RequestBody Activity activity){
-        activity.setTime("2324");
-        activity.setInformation("Hello");
-        return activity;
+
+        service.saveActivity(activity);
+        return activity; // TODO: Retornar o tempo gasto
     }
 
 }
