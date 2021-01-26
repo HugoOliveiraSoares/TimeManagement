2d5d1fb1791828b51c8aaa468c21c6ff55d40c62
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2021-01-26 15:40:32 -0300

    Saves the activity in the database

--- src/main/java/com/hugooliveirasoares/timemanagement/service/Service.java
+++ src/main/java/com/hugooliveirasoares/timemanagement/service/Service.java
@@ -1,5 +1,6 @@
 package com.hugooliveirasoares.timemanagement.service;
 
+import com.hugooliveirasoares.timemanagement.dao.ActivityDAO;
 import com.hugooliveirasoares.timemanagement.domain.Activity;
 import com.hugooliveirasoares.timemanagement.domain.ActivityList;
 
@@ -10,8 +11,14 @@ public class Service {
     ActivityList activityList = new ActivityList();
 
     public void saveActivity(Activity activity){
+        ActivityDAO dao = new ActivityDAO();
+        dao.save(activity);
+//        activityList.getListActivity().add(activity);
+    }
 
-        activityList.getListActivity().add(activity);
+    public List<Activity> getActivities(){
+        ActivityDAO dao = new ActivityDAO();
+        return dao.read();
     }
 
     public ActivityList getActivityList() {
