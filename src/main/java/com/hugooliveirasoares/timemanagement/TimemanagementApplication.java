c9e6def12054e831cd4be0250bd13f9c30c45cb4
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2021-01-26 15:45:56 -0300

    Always create the table in the database when restarting the application

--- src/main/java/com/hugooliveirasoares/timemanagement/TimemanagementApplication.java
+++ src/main/java/com/hugooliveirasoares/timemanagement/TimemanagementApplication.java
@@ -1,15 +1,24 @@
 package com.hugooliveirasoares.timemanagement;
 
+import com.hugooliveirasoares.timemanagement.dao.ActivityDAO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class TimemanagementApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TimemanagementApplication.class, args);
+
+		ActivityDAO activityDAO = new ActivityDAO();
+		activityDAO.connection();
 	}
 
 	@Override
